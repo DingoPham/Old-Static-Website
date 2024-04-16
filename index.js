85fecce4073aa7b83b1d949e3e504b970ced0ec7
DingoPham <pencibambo@gmail.com>
2024-04-17 02:25:46 +0700

    small edit3

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 // Navbar animation
-var lastScroll = window.pageYOffset;
-window.onscroll = function(){
-    var currentScroll = window.pageYOffset;
-
-    if(lastScroll > currentScroll){
-        document.getElementById("nav").style.top = "0";
-    }else{
-        document.getElementById("nav").style.top = "-100px";
-        
-    }
-    lastScroll = currentScroll;
-}
 
 // Navbar point to content head
-document.querySelectorAll('#nav a').forEach(anchor =>{
+document.querySelectorAll('#navBar a').forEach(anchor =>{
     anchor.addEventListener('click', function(e){
         e.preventDefault();
 
