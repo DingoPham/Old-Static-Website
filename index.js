87e5c58ccfe5b966151ba8c3199298e8c0196308
DingoPham <pencibambo@gmail.com>
2024-05-31 23:48:45 +0700

    finished main page

--- index.js
+++ index.js
@@ -87,14 +87,47 @@ overlay.addEventListener("click", function(){
 });
 
 // Light/Dark theme
+// document.getElementById('themeSwitcher').addEventListener('click', function(){
+//     // const themeSwitcher = ;
+//     const moonTheme = document.getElementById('moonTheme');
+//     const sunTheme = document.getElementById('sunTheme');
+//     document.body.classList.toggle('dark-theme');
+
+//     if(themeSwitcher.classList.contains('dark-theme')){
+//         localStorage.setItem('theme', 'dark');
+//         sunTheme.classList.add('hidden');
+//         moonTheme.classList.remove('hidden');
+//     }
+//     else{
+//         localStorage.setItem('theme', 'light');
+//         moonTheme.classList.add('hidden');
+//         sunTheme.classList.remove('hidden');
+//     }
+// });
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const savedTheme = localStorage.getItem('theme');
+//     if(savedTheme && savedTheme === 'dark'){
+//         document.body.classList.add('dark-theme');
+//     }
+// });
+
 document.getElementById('themeSwitcher').addEventListener('click', function(){
+    const moonTheme = document.getElementById('moonTheme');
+    const sunTheme = document.getElementById('sunTheme');
+
+
     document.body.classList.toggle('dark-theme');
 
     if(document.body.classList.contains('dark-theme')){
         localStorage.setItem('theme', 'dark');
+        sunTheme.classList.add('hidden');
+        moonTheme.classList.remove('hidden');
     }
     else{
         localStorage.setItem('theme', 'light');
+        moonTheme.classList.add('hidden');
+        sunTheme.classList.remove('hidden');
     }
 });
 document.addEventListener('DOMContentLoaded', (event) => {
